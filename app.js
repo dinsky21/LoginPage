39e00bec8670f30af69ec8fcf6e2d0f83aca3253
Dinsky21 <dinsky21@gmail.com>
2022-11-29 17:54:09 +0800

    testing express-session

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const methodOverride = require('method-override')
 const routes = require('./routes')
 require('./config/mongoose')
 const PORT = process.env.PORT || 3000
+const session = require('express-session')
 
 //view engine
 app.engine('.hbs', exphbs.engine({ extname: '.hbs', defaultLayout: 'main' }))
@@ -20,6 +21,16 @@ app.use(methodOverride('_method'))
 
 //body-parsing code handle
 app.use(bodyParser.urlencoded({ extended: true }))
+
+//session
+app.use(
+  session({
+    secret: 'mySecret', //存放sessionID
+    name: 'user', // optional，存放在cookie的key
+    saveUninitialized: false,
+    resave: true,
+  })
+)
 
 //routing
 app.use(routes)
--- routes/modules/welcome.js
+++ routes/modules/welcome.js
@@ -1,8 +1,7 @@
 const express = require('express')
+const app = express()
 const router = express.Router()
 const UserSchema = require('../../models/users')
 const PORT = process.env.PORT || 3000
-
-router.post('/', (req, res) => {})
 
 module.exports = router
