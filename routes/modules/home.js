3f9129b4ee888c01d4497bedee85627d655ebf01
Dinsky21 <dinsky21@gmail.com>
2022-11-29 11:33:21 +0800

    feat: add login error message

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,12 +1,28 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+const UserSchema = require('../../models/users')
 const PORT = process.env.PORT || 3000
 // 準備引入路由模組
 
 // 首頁
 router.get('/', (req, res) => {
   res.render('index')
+})
+
+router.post('/', (req, res) => {
+  const { password, email } = req.body
+  UserSchema.findOne({ password, email })
+    .lean()
+    .then((obj) => {
+      if (obj !== null) {
+        res.render('welcome', { user: obj })
+      } else {
+        const message = 'email or password error'
+        res.render('index', { message })
+      }
+    })
+    .catch((error) => console.log(error))
 })
 
 // 匯出路由器
--- routes/modules/welcome.js
+++ routes/modules/welcome.js
@@ -3,18 +3,6 @@ const router = express.Router()
 const UserSchema = require('../../models/users')
 const PORT = process.env.PORT || 3000
 
-router.post('/', (req, res) => {
-  const { password, email } = req.body
-  UserSchema.findOne({ password, email })
-    .lean()
-    .then((obj) => {
-      if (obj !== null) {
-        res.render('welcome', { user: obj })
-      } else {
-        res.redirect('/')
-      }
-    })
-    .catch((error) => console.log(error))
-})
+router.post('/', (req, res) => {})
 
 module.exports = router
